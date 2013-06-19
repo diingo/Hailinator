7023befa8233a548fff6e01857a674b101cfa285
MakerSquare Student <student@themakersquare.com>
2013-06-19 16:18:16 -0500

    Almost complete hailinator. Need figure out how modify CSV filet to have array of arrays instead of array of strings.

--- hailinator.rb
+++ hailinator.rb
@@ -8,16 +8,37 @@ Twitter.configure do |config|
   config.oauth_token_secret = "Qa5tPfyya08Thcd0zlOkhuRwnSYhkW4lAG6UxqK6AE"
 end
 
-tweets = Twitter.search("#hail", options = {:count => 10}).results.map do |tweet|
+tweets = [] 
 
-	tweet.text
+Twitter.search("#hail", options = {:count => 10}).results.each do |tweet|
+
+
+	tweets << [tweet.user.handle, tweet.text, tweet.user.url] 
+
 
 end
 
-# puts tweets[0]
 
-10.times do |x|
-	CSV.open("Hail_tweets.csv","w") do |csv|
-			csv << tweets[x]
-		end
+# puts tweets[0]
+# puts " "
+# puts tweets[9]
+
+# 10.times do |x|
+# 	CSV.open("Hail_tweets.csv","w") do |csv|
+# 		csv << tweets[x]
+# 	end
+# end
+
+CSV.open("Hail_tweets.csv", "w") do |csv|
+	10.times do |tweet|
+		csv << [tweets[tweet].join(",")]
 	end
+end
+
+# our_csv = CSV.generate(row_sep: "\r\n", quote_char: '"', force_quotes: true) do |csv|
+# 	10.times do |tweet|
+# 		csv << [tweets[tweet].join(",")]
+# 	end
+
+# end
+
